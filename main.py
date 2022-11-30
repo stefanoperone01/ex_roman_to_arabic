e868766e248792eece73d6edac10420517dc0333
Stefano Perone <stefanoperone01@AirdiStefano2.homenet.telecomitalia.it>
2022-11-30 15:45:05 +0100

    Implementata la funzione di conversione

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+"""
+Created on Wed Nov 30 15:18:26 2022
+
+@author: stefanoperone01
+"""
+
+def one_char(roman):
+    if len(roman)==1:
+        if roman=='I':
+            roman = 1
+        elif roman == 'V':
+            roman = 5
+        elif roman == 'X':
+            roman = 10
+        elif roman == 'L':
+            roman = 50
+        elif roman == 'C':
+            roman = 100
+        elif roman == 'D':
+            roman = 500
+        elif roman == 'M':
+            roman = 1000
+        elif roman == ' ':
+            raise ValueError('Lo spazio è un carattere non concesso')
+        else:
+            raise ValueError('La lettera '+roman+' non corrisponde a un numero')
+    return int(roman)
+
 def convert_roman_to_arabic(roman: str) -> str:
     """
     convert a string containing a number in roman coding to a
@@ -5,5 +33,28 @@ def convert_roman_to_arabic(roman: str) -> str:
     :param roman: string to be converted
     :return: converted string
     """
-    # inserisci qui il tuo codice
-    pass
+    
+    if len(roman)==1:
+        tot = str(one_char(roman))
+        
+    elif len(roman)>1:
+        number = list(roman)
+        temp = []
+        tot = 0
+        for i in range(len(number)):
+            temp.append(one_char(number[i]))
+            if i == 1:
+                if temp[i-1]<temp[i]:
+                    tot += temp[i]-temp[i-1]
+                else:
+                    tot += temp[i]+temp[i-1]   
+            if i > 1:
+                if temp[i-1]<temp[i] and temp[i-2] > temp[i-1]:
+                    tot += temp[i]-2*temp[i-1]
+                elif temp[i-1]<temp[i]:
+                    tot += temp[i]-temp[i-1]
+                else:
+                    tot += temp[i]
+    return str(tot)
+                    
+                
