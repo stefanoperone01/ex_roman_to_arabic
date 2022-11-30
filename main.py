6c00d41565ac789a1f8f3eb9a6c223881a70c2c0
Stefano Perone <stefanoperone01@AirdiStefano2.homenet.telecomitalia.it>
2022-11-30 17:03:04 +0100

    Implementata la conversione da numeri romani a numeri arabi

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Created on Wed Nov 30 15:18:26 2022
 
 @author: stefanoperone01
 """
-
+#roman = input('Roman: ')
 def one_char(roman):
     if len(roman)==1:
         if roman=='I':
@@ -48,13 +48,20 @@ def convert_roman_to_arabic(roman: str) -> str:
                     tot += temp[i]-temp[i-1]
                 else:
                     tot += temp[i]+temp[i-1]   
-            if i > 1:
-                if temp[i-1]<temp[i] and temp[i-2] > temp[i-1]:
+            elif i > 1:
+                if i > 2:
+                    if temp[i]==temp[i-1]==temp[i-2]==temp[i-3]:
+                        raise ValueError ('Numero romano non esistente')
+                if temp[i-1]<temp[i] and temp[i-2]<temp[i]:
+                    raise ValueError ('Numero romano non esistente')
+                elif temp[i-1]<temp[i] and temp[i-2] > temp[i-1]:
                     tot += temp[i]-2*temp[i-1]
                 elif temp[i-1]<temp[i]:
                     tot += temp[i]-temp[i-1]
                 else:
                     tot += temp[i]
     return str(tot)
+# tot = convert_roman_to_arabic(roman)
+# print(tot)
                     
                 
